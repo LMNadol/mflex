fffba0695eab6cb9105e3592b4c0fa03932b7d71
Lilli Nadol <lmn6@st-andrews.ac.uk>
2024-05-23 13:54:27 +0100

    Nothing important

--- mflex/plot/plot_magnetogram.py
+++ mflex/plot/plot_magnetogram.py
@@ -577,6 +577,291 @@ def plot_fieldlines_soar(
     plt.show()
 
 
+def plot_fieldlines_soar_paper(
+    data_b: np.ndarray[np.float64, np.dtype[np.float64]],
+    h1: float,
+    hmin: float,
+    hmax: float,
+    eps: float,
+    nresol_x: int,
+    nresol_y: int,
+    nresol_z: int,
+    xmin: np.float64,
+    xmax: np.float64,
+    ymin: np.float64,
+    ymax: np.float64,
+    zmin: np.float64,
+    zmax: np.float64,
+    a: float,
+    b: float,
+    alpha: float,
+    stepsize: float = 0.1,
+    view: str = "top",
+    cmap: str = "bone",
+):
+    """
+    Returns 3D plot of photospheric magnetic field including field line extrapolation.
+    """
+
+    data_bz = data_b[:, :, 0, 2]
+    x_arr = np.arange(2 * nresol_x) * (xmax - xmin) / (2 * nresol_x - 1) + xmin
+    y_arr = np.arange(2 * nresol_y) * (ymax - ymin) / (2 * nresol_y - 1) + ymin
+    z_arr = np.arange(nresol_z) * (zmax - zmin) / (nresol_z - 1) + zmin
+    x_grid, y_grid = np.meshgrid(x_arr, y_arr)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    ax.contourf(
+        x_grid[nresol_y : 2 * nresol_y, nresol_x : 2 * nresol_x],
+        y_grid[nresol_y : 2 * nresol_y, nresol_x : 2 * nresol_x],
+        data_bz[nresol_y : 2 * nresol_y, nresol_x : 2 * nresol_x],
+        1000,
+        cmap=cmap,
+        offset=0.0,
+    )
+    # Have to have Xgrid first, Ygrid second, as Contourf expects x-axis/ columns first, then y-axis/rows
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")  # type: ignore
+    ax.set_zlim([zmin, zmax])  # type: ignore
+
+    if view == "top":
+        ax.view_init(90, -90)  # type: ignore
+        ax.set_zticklabels([])  # type: ignore
+    if view == "angular":
+        ax.view_init(30, 240, 0)  # type: ignore
+    if view == "side":
+        ax.view_init(0, -90)  # type: ignore
+        ax.set_yticklabels([])  # type: ignore
+
+    ax.set_box_aspect((xmax, ymax, zmax))  # type: ignore
+
+    x_0 = 1.0 * 10**-8
+    y_0 = 1.0 * 10**-8
+    dx = stepsize
+    dy = stepsize
+    nlinesmaxx = math.floor(xmax / dx)
+    nlinesmaxy = math.floor(ymax / dy)
+
+    # Limit fieldline plot to original data size (rather than Seehafer size)
+    boxedges = np.zeros((2, 3))
+
+    # Y boundaries must come first, X second due to switched order explained above
+    boxedges[0, 0] = 0.0
+    boxedges[1, 0] = ymax
+    boxedges[0, 1] = 0.0
+    boxedges[1, 1] = xmax
+    boxedges[0, 2] = zmin
+    boxedges[1, 2] = zmax
+
+    for ilinesx in range(0, nlinesmaxx):
+        for ilinesy in range(0, nlinesmaxy):
+            x_start = x_0 + dx * ilinesx
+            y_start = y_0 + dy * ilinesy
+
+            if data_bz[int(y_start), int(x_start)] < 0.0:
+                h1 = -h1
+
+            ystart = [y_start, x_start, 0.0]
+            # Fieldline3D expects startpt, BField, Row values, Column values so we need to give Y first, then X
+            fieldline = fieldline3d(
+                ystart,
+                data_b,
+                y_arr,
+                x_arr,
+                z_arr,
+                h1,
+                hmin,
+                hmax,
+                eps,
+                oneway=False,
+                boxedge=boxedges,
+                gridcoord=False,
+                coordsystem="cartesian",
+            )  # , periodicity='xy')
+
+            # Plot fieldlines
+            fieldline_x = np.zeros(len(fieldline))
+            fieldline_y = np.zeros(len(fieldline))
+            fieldline_z = np.zeros(len(fieldline))
+            fieldline_x[:] = fieldline[:, 0]
+            fieldline_y[:] = fieldline[:, 1]
+            fieldline_z[:] = fieldline[:, 2]
+
+            # Need to give row direction first/ Y, then column direction/ X
+            ax.plot(
+                fieldline_y,
+                fieldline_x,
+                fieldline_z,
+                color="magenta",
+                linewidth=0.25,
+                zorder=4000,
+            )
+
+    current_time = datetime.now()
+    dt_string = current_time.strftime("%d-%m-%Y_%H-%M-%S")
+
+    plotname = (
+        "/Users/lilli/Desktop/Paper/SOAR1_"
+        + str(a)
+        + "_"
+        + str(b)
+        + "_"
+        + str(alpha)
+        + "_"
+        + view
+        + "_"
+        + dt_string
+        + ".png"
+    )
+    plt.savefig(plotname, dpi=300)
+
+    plt.show()
+
+
+def plot_fieldlines_soar_paper_zoom(
+    data_b: np.ndarray[np.float64, np.dtype[np.float64]],
+    h1: float,
+    hmin: float,
+    hmax: float,
+    eps: float,
+    nresol_x: int,
+    nresol_y: int,
+    nresol_z: int,
+    xmin: np.float64,
+    xmax: np.float64,
+    ymin: np.float64,
+    ymax: np.float64,
+    zmin: np.float64,
+    zmax: np.float64,
+    z0: np.float64,
+    a: float,
+    b: float,
+    alpha: float,
+    stepsize: float = 0.1,
+    view: str = "top",
+    cmap: str = "bone",
+):
+    """
+    Returns 3D plot of photospheric magnetic field including field line extrapolation.
+    """
+
+    data_bz = data_b[:, :, 0, 2]
+    x_arr = np.arange(2 * nresol_x) * (xmax - xmin) / (2 * nresol_x - 1) + xmin
+    y_arr = np.arange(2 * nresol_y) * (ymax - ymin) / (2 * nresol_y - 1) + ymin
+    z_arr = np.arange(nresol_z) * (zmax - zmin) / (nresol_z - 1) + zmin
+    x_grid, y_grid = np.meshgrid(x_arr, y_arr)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    ax.contourf(
+        x_grid[nresol_y : 2 * nresol_y, nresol_x : 2 * nresol_x],
+        y_grid[nresol_y : 2 * nresol_y, nresol_x : 2 * nresol_x],
+        data_bz[nresol_y : 2 * nresol_y, nresol_x : 2 * nresol_x],
+        1000,
+        cmap=cmap,
+        offset=0.0,
+    )
+    # Have to have Xgrid first, Ygrid second, as Contourf expects x-axis/ columns first, then y-axis/rows
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")  # type: ignore
+    ax.set_zlim([zmin, 2 * z0])  # type: ignore
+
+    if view == "top":
+        ax.view_init(90, -90)  # type: ignore
+        ax.set_zticklabels([])  # type: ignore
+    if view == "angular":
+        ax.view_init(30, 240, 0)  # type: ignore
+    if view == "side":
+        ax.view_init(0, -90)  # type: ignore
+        ax.set_yticklabels([])  # type: ignore
+
+    ax.set_box_aspect((xmax, ymax, 4 * z0))  # type: ignore
+
+    x_0 = 1.0 * 10**-8
+    y_0 = 1.0 * 10**-8
+    dx = stepsize
+    dy = stepsize
+    nlinesmaxx = math.floor(xmax / dx)
+    nlinesmaxy = math.floor(ymax / dy)
+
+    # Limit fieldline plot to original data size (rather than Seehafer size)
+    boxedges = np.zeros((2, 3))
+
+    # Y boundaries must come first, X second due to switched order explained above
+    boxedges[0, 0] = 0.0
+    boxedges[1, 0] = ymax
+    boxedges[0, 1] = 0.0
+    boxedges[1, 1] = xmax
+    boxedges[0, 2] = zmin
+    boxedges[1, 2] = 2 * z0
+
+    for ilinesx in range(0, nlinesmaxx):
+        for ilinesy in range(0, nlinesmaxy):
+            x_start = x_0 + dx * ilinesx
+            y_start = y_0 + dy * ilinesy
+
+            if data_bz[int(y_start), int(x_start)] < 0.0:
+                h1 = -h1
+
+            ystart = [y_start, x_start, 0.0]
+            # Fieldline3D expects startpt, BField, Row values, Column values so we need to give Y first, then X
+            fieldline = fieldline3d(
+                ystart,
+                data_b,
+                y_arr,
+                x_arr,
+                z_arr,
+                h1,
+                hmin,
+                hmax,
+                eps,
+                oneway=False,
+                boxedge=boxedges,
+                gridcoord=False,
+                coordsystem="cartesian",
+            )  # , periodicity='xy')
+
+            # Plot fieldlines
+            fieldline_x = np.zeros(len(fieldline))
+            fieldline_y = np.zeros(len(fieldline))
+            fieldline_z = np.zeros(len(fieldline))
+            fieldline_x[:] = fieldline[:, 0]
+            fieldline_y[:] = fieldline[:, 1]
+            fieldline_z[:] = fieldline[:, 2]
+
+            # Need to give row direction first/ Y, then column direction/ X
+            ax.plot(
+                fieldline_y,
+                fieldline_x,
+                fieldline_z,
+                color="magenta",
+                linewidth=0.25,
+                zorder=4000,
+            )
+
+    current_time = datetime.now()
+    dt_string = current_time.strftime("%d-%m-%Y_%H-%M-%S")
+
+    plotname = (
+        "/Users/lilli/Desktop/Paper/SOAR1_"
+        + str(a)
+        + "_"
+        + str(b)
+        + "_"
+        + str(alpha)
+        + "_"
+        + view
+        + "_"
+        + dt_string
+        + ".png"
+    )
+    plt.savefig(plotname, dpi=300)
+
+    plt.show()
+
+
 def plot_fieldlines_polar(
     data_b: np.ndarray[np.float64, np.dtype[np.float64]],
     h1: float,
@@ -1669,10 +1954,10 @@ def plot_fieldlines_polar_paper2(
                 )
 
     ax.set_zlim([zmin, zmax])  # type: ignore
-    #ax.view_init(0, -90)  # type: ignore
+    # ax.view_init(0, -90)  # type: ignore
     ax.view_init(30, -115, 0)  # type: ignore
-    #ax.set_yticks([])
-    #ax.set_yticklabels([])
+    # ax.set_yticks([])
+    # ax.set_yticklabels([])
     plotname = (
         "/Users/lilli/Desktop/Paper/" + name + "_" + str(a) + "_" + str(alpha) + ".png"
     )
