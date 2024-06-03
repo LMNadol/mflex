07f36fea8bb930e33ad2913f40b9c317a4e6c991
Lilli Nadol <lmn6@st-andrews.ac.uk>
2024-06-03 15:33:47 +0100

    Paper

--- mflex/plot/plot_magnetogram.py
+++ mflex/plot/plot_magnetogram.py
@@ -4,7 +4,7 @@ from mflex.plot.linetracer.fieldline3D import fieldline3d
 from datetime import datetime
 import math
 from mflex.plot.linetracer.linecheck import fieldlinecheck
-import matplotlib
+from matplotlib import colors
 
 
 def plot_magnetogram_boundary(
@@ -558,21 +558,21 @@ def plot_fieldlines_soar(
     current_time = datetime.now()
     dt_string = current_time.strftime("%d-%m-%Y_%H-%M-%S")
 
-    plotname = (
-        "/Users/lilli/Desktop/mflex/seminar2024/SOAR_BL2_"
-        + str(a)
-        + "_"
-        + str(b)
-        + "_"
-        + str(alpha)
-        + "_"
-        + view
-        + "_"
-        + dt_string
-        + ".png"
-    )
-    ax.set_zticklabels([])  # type: ignore
-    plt.savefig(plotname, dpi=300)
+    # plotname = (
+    #     "/Users/lilli/Desktop/mflex/seminar2024/SOAR_BL2_"
+    #     + str(a)
+    #     + "_"
+    #     + str(b)
+    #     + "_"
+    #     + str(alpha)
+    #     + "_"
+    #     + view
+    #     + "_"
+    #     + dt_string
+    #     + ".png"
+    # )
+    # ax.set_zticklabels([])  # type: ignore
+    # plt.savefig(plotname, dpi=300)
 
     plt.show()
 
@@ -703,6 +703,165 @@ def plot_fieldlines_soar_paper(
 
     plotname = (
         "/Users/lilli/Desktop/Paper/SOAR1_"
+        + str(a)
+        + "_"
+        + str(b)
+        + "_"
+        + str(alpha)
+        + "_"
+        + view
+        + "_"
+        + dt_string
+        + ".png"
+    )
+    plt.savefig(plotname, dpi=300)
+
+    plt.show()
+
+
+def plot_fieldlines_sdo_paper(
+    data_b: np.ndarray[np.float64, np.dtype[np.float64]],
+    h1: float,
+    hmin: float,
+    hmax: float,
+    eps: float,
+    nresol_x: int,
+    nresol_y: int,
+    nresol_z: int,
+    xmin: np.float64,
+    xmax: np.float64,
+    ymin: np.float64,
+    ymax: np.float64,
+    zmin: np.float64,
+    zmax: np.float64,
+    a: float,
+    b: float,
+    alpha: float,
+    stepsize: float = 0.1,
+    view: str = "top",
+    cmap: str = "bone",
+):
+    """
+    Returns 3D plot of photospheric magnetic field including field line extrapolation.
+    """
+
+    data_bz = data_b[:, :, 0, 2]
+    x_arr = np.arange(2 * nresol_x) * (xmax - xmin) / (2 * nresol_x - 1) + xmin
+    y_arr = np.arange(2 * nresol_y) * (ymax - ymin) / (2 * nresol_y - 1) + ymin
+    z_arr = np.arange(nresol_z) * (zmax - zmin) / (nresol_z - 1) + zmin
+    x_grid, y_grid = np.meshgrid(x_arr, y_arr)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    norm = colors.SymLogNorm(50, vmin=-7.5e2, vmax=7.5e2)  # type : ignore
+    ax.contourf(
+        x_grid[nresol_y : 2 * nresol_y, nresol_x : 2 * nresol_x],
+        y_grid[nresol_y : 2 * nresol_y, nresol_x : 2 * nresol_x],
+        data_bz[nresol_y : 2 * nresol_y, nresol_x : 2 * nresol_x],
+        1000,
+        norm=norm,
+        cmap=cmap,
+        offset=0.0,
+    )
+    # Have to have Xgrid first, Ygrid second, as Contourf expects x-axis/ columns first, then y-axis/rows
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")  # type: ignore
+    ax.set_zlim([zmin, zmax])  # type: ignore
+
+    if view == "top":
+        ax.view_init(90, -90)  # type: ignore
+        ax.set_zticklabels([])  # type: ignore
+        ax.set_zlabel("")  # type: ignore
+        ax.set_xlabel("x", labelpad=15)
+    if view == "angular":
+        ax.view_init(30, 240, 0)  # type: ignore
+    if view == "side":
+        ax.view_init(0, -90)  # type: ignore
+        ax.set_yticklabels([])  # type: ignore
+        ax.set_xlabel("x", labelpad=30)
+        ax.set_ylabel("")
+        ax.set_zticks(np.arange(0, zmax + 1, 5))  # type: ignore
+
+    ax.set_box_aspect((xmax, ymax, 3 * zmax))  # type: ignore
+
+    x_0 = 1.0 * 10**-8
+    y_0 = 1.0 * 10**-8
+    dx = stepsize
+    dy = stepsize
+    nlinesmaxx = math.floor(xmax / dx)
+    nlinesmaxy = math.floor(ymax / dy)
+
+    # Limit fieldline plot to original data size (rather than Seehafer size)
+    boxedges = np.zeros((2, 3))
+
+    # Y boundaries must come first, X second due to switched order explained above
+    boxedges[0, 0] = 0.0
+    boxedges[1, 0] = ymax
+    boxedges[0, 1] = 0.0
+    boxedges[1, 1] = xmax
+    boxedges[0, 2] = zmin
+    boxedges[1, 2] = zmax
+
+    for ilinesx in range(0, nlinesmaxx):
+        for ilinesy in range(0, nlinesmaxy):
+            x_start = x_0 + dx * ilinesx
+            y_start = y_0 + dy * ilinesy
+
+            if data_bz[int(y_start), int(x_start)] < 0.0:
+                h1 = -h1
+
+            ystart = [y_start, x_start, 0.0]
+            # Fieldline3D expects startpt, BField, Row values, Column values so we need to give Y first, then X
+            fieldline = fieldline3d(
+                ystart,
+                data_b,
+                y_arr,
+                x_arr,
+                z_arr,
+                h1,
+                hmin,
+                hmax,
+                eps,
+                oneway=False,
+                boxedge=boxedges,
+                gridcoord=False,
+                coordsystem="cartesian",
+            )  # , periodicity='xy')
+
+            # Plot fieldlines
+            fieldline_x = np.zeros(len(fieldline))
+            fieldline_y = np.zeros(len(fieldline))
+            fieldline_z = np.zeros(len(fieldline))
+            fieldline_x[:] = fieldline[:, 0]
+            fieldline_y[:] = fieldline[:, 1]
+            fieldline_z[:] = fieldline[:, 2]
+
+            if np.isclose(fieldline_z[-1], 0.0) and np.isclose(fieldline_z[0], 0.0):
+                # Need to give row direction first/ Y, then column direction/ X
+                ax.plot(
+                    fieldline_y,
+                    fieldline_x,
+                    fieldline_z,
+                    color="aqua",
+                    linewidth=0.5,
+                    zorder=4000,
+                )
+            else:
+                ax.plot(
+                    fieldline_y,
+                    fieldline_x,
+                    fieldline_z,
+                    color="dodgerblue",
+                    linewidth=0.5,
+                    zorder=4000,
+                )
+
+    current_time = datetime.now()
+    dt_string = current_time.strftime("%d-%m-%Y_%H-%M-%S")
+
+    plotname = (
+        "/Users/lilli/Desktop/Paper/hmi_m_45s_2024_05_07_07_31_30_tai_magnetogram_mflex_"
         + str(a)
         + "_"
         + str(b)
