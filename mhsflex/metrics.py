26359b6ee07521b61fc8b7d6ef12884506c7e145
Lilli Nadol <lmn6@st-andrews.ac.uk>
2024-10-01 16:45:57 +0100

    thesis

--- mhsflex/metrics.py
+++ mhsflex/metrics.py
@@ -754,6 +754,178 @@ def field_div_metric_issi(
     return np.float64(count / count_closed)
 
 
+def field_div_metric_issi2(
+    B_ref: np.ndarray,
+    datab: Field3dData,
+) -> np.float64:
+    """
+    Returns Field Line Divergence metric of B : B_ref and b : B_rec.
+    xmin, ymin, ymax as when using plot_fieldline_grid.
+
+    Tracing field lines from footpoints on a grid on the bottom boundary in both reference model
+    and reconstruction. If both field lines end again on the bottom boundary, a score is assigned:
+    the distance between the two endpoints divided by the length of the field line in the reference model.
+    Then the metric score can be obtained by the fraction of the initial footpoints in which these scores
+    are less than 10%.
+    """
+    
+  
+    h1 = 1.0 / 100.0  # Initial step length for fieldline3D
+    eps = 1.0e-8
+    # Tolerance to which we require point on field line known for fieldline3D
+    hmin = 0.0  # Minimum step length for fieldline3D
+    hmax = 1.0  # Maximum step length for fieldline3D
+
+    xmin, xmax, ymin, ymax, zmin, zmax = (
+        datab.x[280],
+        datab.x[490],
+        datab.y[60],
+        datab.y[240],
+        datab.z[0],
+        datab.z[469],
+    )
+
+    if datab.field[60:240, 280:490, 0:469, :].shape != B_ref.shape:
+        raise ValueError("Fields not of same size.")
+
+    z_arr = np.arange(datab.nz) * (zmax - zmin) / (datab.nz - 1) + zmin
+
+    # Limit fieldline plot to original data size (rather than Seehafer size)
+    boxedges = np.zeros((2, 3))
+
+    # Y boundaries must come first, X second due to switched order explained above
+    boxedges[0, 0] = ymin
+    boxedges[1, 0] = ymax
+    boxedges[0, 1] = xmin
+    boxedges[1, 1] = xmax
+    boxedges[0, 2] = zmin
+    boxedges[1, 2] = zmax
+
+    # set fieldline3D initial stepsize
+    h1_ref = h1
+    h1_rec = h1
+
+    # counter for number of footpoints that have an error in the endpoints smaller than 10 percent of field line length in reference model
+    count = 0
+
+    # counter for number of field lines that are closed within box
+    count_closed = 0
+
+    # counter for all field lines
+    count_all = 0
+
+    for ix in datab.x[330:440]:
+        for iy in datab.y[145:210]:
+
+            count_all = count_all + 1
+
+            # Footpoint
+            x_start = ix
+            y_start = iy
+            ystart = [y_start, x_start, 0.0]
+
+            # decide direction of fieldline for reference model
+            if B_ref[int(y_start), int(x_start), 0, 2] < 0.0:
+                h1_ref = -h1_ref
+            # decide direction of fieldline for reconstruction model
+            if datab.field[int(y_start), int(x_start), 0, 2] < 0.0:
+                h1_rec = -h1_rec
+
+            # Fieldline3D expects startpt, BField, Row values, Column values so we need to give Y first, then X
+
+            # Get fieldline coordinates for reference model
+            fieldline_ref = fieldline3d(
+                ystart,
+                B_ref,
+                datab.y,
+                datab.x,
+                z_arr,
+                h1_ref,
+                hmin,
+                hmax,
+                eps,
+                oneway=False,
+                boxedge=boxedges,
+                gridcoord=False,
+                coordsystem="cartesian",
+            )
+
+            # Get fieldline coordinates for reconstruction model
+            fieldline_rec = fieldline3d(
+                ystart,
+                datab.field,
+                datab.y,
+                datab.x,
+                z_arr,
+                h1_rec,
+                hmin,
+                hmax,
+                eps,
+                oneway=False,
+                boxedge=boxedges,
+                gridcoord=False,
+                coordsystem="cartesian",
+            )
+
+            len_ref = len(fieldline_ref)
+            len_rec = len(fieldline_rec)
+
+            valid_fieldline = True
+
+            # Check if field lines end on bottom boundary
+            if not np.isclose(fieldline_ref[len_ref - 1, 2], 0.0):
+                valid_fieldline = False
+            if not np.isclose(fieldline_rec[len_rec - 1, 2], 0.0):
+                valid_fieldline = False
+
+            if not (0.0 <= fieldline_ref[len_ref - 1, 1] <= xmax):
+                valid_fieldline = False
+            if not (0.0 <= fieldline_rec[len_rec - 1, 1] <= xmax):
+                valid_fieldline = False
+            if not (0.0 <= fieldline_ref[len_ref - 1, 0] <= ymax):
+                valid_fieldline = False
+            if not (0.0 <= fieldline_rec[len_rec - 1, 0] <= ymax):
+                valid_fieldline = False
+
+            if valid_fieldline:
+
+                # add to counter if field line is closed field line within box
+                count_closed = count_closed + 1
+
+                # calculate distance between the endpoints of reference field line and of reconstructed field line
+                num = np.sqrt(
+                    (fieldline_rec[len_rec - 1, 1] - fieldline_ref[len_ref - 1, 1])
+                    ** 2.0
+                    + (fieldline_rec[len_rec - 1, 0] - fieldline_ref[len_ref - 1, 0])
+                    ** 2.0
+                    + (fieldline_rec[len_rec - 1, 2] - fieldline_ref[len_ref - 1, 2])
+                    ** 2.0
+                )
+
+                # calculate length of reference field line
+                div = 0.0
+                for i in range(0, len_ref - 1):
+                    div = div + np.sqrt(
+                        (fieldline_ref[i, 1] - fieldline_ref[i + 1, 1]) ** 2.0
+                        + (fieldline_ref[i, 0] - fieldline_ref[i + 1, 0]) ** 2.0
+                        + (fieldline_ref[i, 2] - fieldline_ref[i + 1, 2]) ** 2.0
+                    )
+
+                # divide distance between endpoints by length of reference field line
+                # gives error between endpoints as percentage of length of reference field line
+
+                temp = num / div
+
+                # add to counter if error is smaller than 10 percent
+                if temp <= 0.1:
+                    count = count + 1
+
+    print(count_all, count_closed, count)
+
+    # return number of footpoints with error smaller than 10 percent as percentage of all footpoints, of all closed fieldlines
+    return np.float64(count / count_closed)
+
+
 def pearson_corr_coeff_issi(
     fpres_3d_ref: np.ndarray,
     fden_3d_ref: np.ndarray,
@@ -833,16 +1005,15 @@ def pearson_corr_coeff_issi_2(
     for both pressure and density between the reference and the recreated model.
     """
 
-    zmin, zmax = datab.z[0], datab.z[-1]
-    z_arr = np.arange(datab.nz) * (zmax - zmin) / (datab.nz - 1) + zmin
+    z_arr = datab.z[0:469]
 
     pres_surface_ref = np.zeros((datab.ny, datab.nx))
     den_surface_ref = np.zeros((datab.ny, datab.nx))
     pres_surface_rec = np.zeros((datab.ny, datab.nx))
     den_surface_rec = np.zeros((datab.ny, datab.nx))
 
-    for ix in range(datab.nx):
-        for iy in range(datab.ny):
+    for ix in range(fpres_3d_ref.shape[1]):
+        for iy in range(fpres_3d_ref.shape[0]):
             pres_surface_ref[iy, ix] = np.trapz(fpres_3d_ref[iy, ix, :], z_arr)
             den_surface_ref[iy, ix] = np.trapz(fden_3d_ref[iy, ix, :], z_arr)
             pres_surface_rec[iy, ix] = np.trapz(fpres_3d_rec[iy, ix, :], z_arr)
